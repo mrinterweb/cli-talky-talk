5b09d594bd10e5631761eac0d6e99d94d195aa06
Sean McCleary <seanmcc@gmail.com>
2015-10-01 18:11:47 -0700

    adding preferences yml file

--- lib/cli-talky-talk.rb
+++ lib/cli-talky-talk.rb
@@ -1,66 +1,71 @@
 require 'pry'
+require 'yaml'
+
 class CliTalkyTalk
 
-  attr_accessor :options
+  attr_accessor :options, :preferences
 
   def initialize(options)
+    begin
+      default_path = File.expand_path('../../', __FILE__) + '/config/preferences.yml'
+      if File.exist? default_path
+        @preferences = YAML.load_file(default_path)
+      else
+        exit "unable to load preferences"
+      end
+    rescue Psych::SyntaxError => e
+      exit "There was an error parsing the config file: #{e}"
+    end
     @options = options || OpenStruct.new
   end
 
-  VOICES = `say -v \?`.split("\n").select { |v| v =~ /en_/ }.map { |v| v.gsub(/\s+en_US.*$/,"")}
-
-  GREETINGS = [
-    'ahoy', 
-    'greeting',
-    'saalutations',
-    'waazame',
-    'bleep booop',
-    'exceslior',
-    'land hoe',
-    'hail',
-  ]
-
-  NAMES = [
-    `whoami`.chomp,
-    'Govna',
-    'captain',
-    'boss',
-    'cheef',
-    'over loard',
-    'dragon commander',
-    'your excellence',
-    'exhaulted one',
-  ]
-
-  STATUSES = [
-    'all done here',
-    'works done',
-    'bingo!',
-    'hazaa',
-    'flip Yeah',
-    'get back to work',
-    'all systems go',
-  ]
 
   def rand(arr)
     arr.sample
   end
 
   def speak
-    voice = options.voice || rand(VOICES)
-    log "voice: #{voice}"
     sentence = options.sentence || random_sentence
     log "sentence: #{sentence}"
     `say -v#{voice} #{sentence}`
   end
 
   def random_sentence
-    "#{rand GREETINGS}, #{rand NAMES}!   #{rand STATUSES}"
+    "#{get :greetings}, #{get :names}!   #{get :statuses}"
   end
 
   def log(str)
     if options and options.debug
       puts str
     end
+  end
+
+  def get(key)
+    str = rand preferences['segments'][key.to_s]
+    # convert for specific keyword tokens
+    case str
+    when 'WHOAMI'
+      `whoami`.chomp
+    else
+      str
+    end
+  end
+
+  def voice
+    voice_opts = preferences['options']['voice']
+    whitelist = voice_opts['whitelist']
+    blacklist = voice_opts['blacklist']
+
+    voice = if options.voice
+      options.voice
+    elsif whitelist.kind_of?(Array)
+      rand whitelist
+    else
+      voices = `say -v \?`.split("\n").select { |v| v =~ /en_/ }.map { |v| v.gsub(/\s+en_[A-Z]{2}.*$/,"")}
+      voices -= blacklist if blacklist.kind_of?(Array)
+      rand voices
+    end
+    log "voice: #{voice}"
+    voice
   end
 end
